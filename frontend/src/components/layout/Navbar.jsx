1ec52a2d0c7acba9d5dcb79db0efbce6a0d57444
sipra <m.moawizsipra@gmail.com>
2025-08-12 06:59:04 -0700

    making hersection with styling and making it's components

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -4,11 +4,13 @@ import {Link} from 'react-router-dom';
 
 const Navbar = () => {
   return (
-   <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-3">
-  <Link className="nav-link text black" to="/">Home</Link>
-  <Link className="nav-link text black" to="/CreateBook">CreateBook</Link>
-  <Link className="nav-link text black" to="/DeleteBook">DeleteBook</Link>
-  <Link className="nav-link text black" to="/Addbook">AddBook</Link>
+   <nav className="navbar navbar-expand-lg navbar-dark bg-purple-400 p-3">
+    <dv className="containcer-fluid">
+  <Link className="nav-link text-white" to="/">Home</Link>
+  <Link className="nav-link text-white" to="/CreateBook">CreateBook</Link>
+  <Link className="nav-link text-white" to="/DeleteBook">DeleteBook</Link>
+  <Link className="nav-link text-white" to="/Addbook">AddBook</Link>
+</dv>
 </nav>
 
   );
